cc036387022f901f3c14c0f396644deb981759fc
Mithilesh Mohanty <mithilesh.mohanty@gmail.com>
2018-05-20 19:02:23 +0530

    Changes for pulling python code from rawgit
    Use of raw git

--- TextAnalytics/GroupAssignment-1/TechnologyFirmAnalysis.py
+++ TextAnalytics/GroupAssignment-1/TechnologyFirmAnalysis.py
@@ -2,10 +2,26 @@ import subprocess
 import feather
 import pandas as pd
 import os
-from CleanText import cleanTextBlock
-from CleanText import textCharacterstics
-from Dtm import createDTM
-from Dtm import generateDTMDifference
+from urllib.request import urlopen
+
+data = urlopen("https://cdn.rawgit.com/mithileshmohanty/ISBAnalytics/174aa8c0/TextAnalytics/GroupAssignment-1/CleanText.py")
+f=open('CleanText.py', "w") 
+t="".join(map(chr, data.read()))
+f.write(t)
+f.close()
+data = urlopen("https://cdn.rawgit.com/mithileshmohanty/ISBAnalytics/174aa8c0/TextAnalytics/GroupAssignment-1/PlotGraph.py")
+f=open('PlotGraph.py', "w") 
+t="".join(map(chr, data.read()))
+f.write(t)
+f.close()
+data = urlopen("https://cdn.rawgit.com/mithileshmohanty/ISBAnalytics/174aa8c0/TextAnalytics/GroupAssignment-1/Dtm.py")
+f=open('Dtm.py', "w") 
+t="".join(map(chr, data.read()))
+f.write(t)
+f.close()
+
+ct = __import__("CleanText")
+d = __import__("Dtm")
 
 if not os.path.exists(".\\Output"):
     os.makedirs(".\\Output")
@@ -15,11 +31,11 @@ documentDictionary={}
 for year in range(2005,2015):
     corporaList=[]
     for index,row in feather.read_dataframe(".\\Temp\\"+str(year)+".feather").iterrows():
-        #textCharacterstics(row["bd.text"])
-        corporaList.append(cleanTextBlock(row["bd.text"]))
+        #ct.textCharacterstics(row["bd.text"])
+        corporaList.append(ct.cleanTextBlock(row["bd.text"]))
     os.remove(".\\Temp\\"+str(year)+".feather")
     print("Creating DTM for Year: %s" %year)
-    createDTM(corporaList,'.\\Output\\'+str(year))
+    d.createDTM(corporaList,'.\\Output\\'+str(year))
     corporaList.clear()
     print("completed DTM for Year: %s" %year)
 
@@ -27,7 +43,7 @@ df_dict = pd.read_csv('.\\Output\\2005.tsv', sep='\t')
 
 for year in range(2006,2015):
     df_new = pd.read_csv('.\\Output\\'+str(year)+'.tsv', sep='\t')
-    df=generateDTMDifference(df_dict,df_new,'.\\Output\\'+str(year)+"_New")
+    df=d.generateDTMDifference(df_dict,df_new,'.\\Output\\'+str(year)+"_New")
     print(len(df_dict))
     print(len(df_new))
     df_dict=df_dict.append(df)
